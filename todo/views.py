ce7a768ecf4561cc3711e564e214cb44557d8a75
ryan-in-thailand <ryanastaphan@gmail.com>
2021-06-28 08:57:13 +0700

    1st commit

---
+++ todo/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+
+from .models import Todo
+from .forms import TodoForm
+
+# Create your views here.
+def todo_list(request):
+    todos = Todo.objects.all()
+    context = {
+        'todo_list': todos
+    }
+    return render(request, 'todo/todo_list.html', context)
+
+def todo_create(request):
+    form = TodoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        # name = form.cleaned_data['name']
+        # due_date = form.cleaned_data['due_date']
+        # print(name, due_date)
+
+        # new_todo = Todo.objects.create(name=name, due_date=due_date)
+        return redirect('/')
+    context = {'form': form}
+    return render(request, 'todo/todo_create.html', context)
+
+# Read
+def todo_detail(request, id):
+    todo = Todo.objects.get(id=id)
+    context = {
+        'todo': todo
+    }
+    return render(request, 'todo/todo_detail.html', context)
+
+# Update
+def todo_update(request,id):
+    todo = Todo.objects.get(id=id)
+    form = TodoForm(request.POST or None, instance=todo)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    context = {'form': form}
+    return render(request, 'todo/todo_update.html', context)
+
+# Delete
+def todo_delete(request, id):
+    todo = Todo.objects.get(id=id)
+    todo.delete()
+    return redirect('/')
